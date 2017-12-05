d8402c7a29e30a89eec0c7e0ad7282ebb4d34f9b
Andrew <wregea63@uwosh.edu>
2017-12-05 09:22:06 -0600

    Created show active question php page

--- Public/PHP/showActive.php
+++ Public/PHP/showActive.php
@@ -14,10 +14,62 @@
       <?php include 'header.php' ?>
       <div id="flexContainer">
         <?php 
-        $question = display_Q_table()[0]; 
+        $question = get_active()[0]; 
         $answers = get_question_answers($question['QuestionId']);
-        ?>
 
+        if ($question != null) {?>
+        <form action="submitAnswer.php" method="post">
+          <input type="hidden" name="QuestionId" value="<?php echo $question['QuestionId'] ?>" >
+          <p> <?php echo $question['QuestionText']; ?> </p>
+
+          <?php if ($question['QuestionType'] === "multiple") { 
+            $i = 0;
+             foreach ($answers as $answer) { ?>
+               <input type="radio" id="radio<?php echo $i;?>" name="radio" value="<?php echo $answer['AnswerText'];?>">
+               <label for="radio<?php echo $i;?>"> <?php echo $answer['AnswerText'];?> </label>
+              <?php $i = $i + 1;
+             }
+           } ?>
+
+           <?php else if ($question['QuestionType'] === "checkbox") { 
+            $i = 0;
+             foreach ($answers as $answer) { ?>
+               <input type="checkbox" id="checkbox<?php echo $i;?>" name="checkbox" value="<?php echo $answer['AnswerText'];?>">
+               <label for="checkbox<?php echo $i;?>"> <?php echo "$i. " + $answer['AnswerText'];?> </label>
+              <?php $i = $i + 1;
+             }
+           } ?>
+
+           <?php else if ($question['QuestionType'] === "short") { 
+            $i = 0;
+             foreach ($answers as $answer) { ?>
+               <input type="text" id="text<?php echo $i;?>" name="text">
+               <label for="text<?php echo $i;?>"> <?php echo $answer['AnswerText'];?> </label>
+              <?php $i = $i + 1;
+             }
+           } ?>
+          <input type="submit" value="Submit">
+        </form>
+
+        <?php } else { ?>
+          <div id="errorMessage">
+            Let the instructor know that there are no active questions in the
+            database.
+          </div>
+          <div id="studentHomePage">
+            <p>
+              Are you currently in class? Has the instructor activated the next
+              question?<br/>
+              Then click this button right away.
+            </p>
+            <form action="showActive.php" id="showQuestion" class="questions">
+              <div>
+                <input type="submit" value="Show Question"/>
+              </div>
+            </form>
+          </div>
+        
+        <?php } ?>
       </div>
     </div>
     <?php include 'footer.php';?>
