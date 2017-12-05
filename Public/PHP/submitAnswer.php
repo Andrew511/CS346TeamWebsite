e0dec1c7bcf3581f8971d03a807ebc7890922c5a
Andrew <wregea63@uwosh.edu>
2017-12-05 09:22:43 -0600

    Created submit question php page, with automatic grading

---
+++ Public/PHP/submitAnswer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<?php    require_once('../../Private/PHP/initialize.php'); ?>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>UWO WebCLICKER</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/p1indiva.css" />
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface"
+      rel="stylesheet"/>
+  </head>
+  <body>
+    <?php include 'student_navigation.php';?>
+    <div class="border">
+      <?php include 'header.php' ?>
+      <div id="flexContainer">
+        <?php $question = get_question($_POST['QuestionId']);
+        $answers = get_question_answers($question['QuestionId']);
+        $score = 1;
+        $totalScore = $question['PointsAvailable'];
+        $numcorrect = 0;
+        $correctAnswers = "";
+        foreach ($answers as $answer) {
+         if ($answer['Correct'] === 1) {
+            $numcorrect = $numcorrect + 1;
+         }   
+        }
+        $scorePerCorrect = ($totalScore - 1) / $numcorrect;
+         if ($question['QuestionType'] === "multiple") { 
+            for ($i = 0; $i < count($answers); $i += 1 ) {
+               if ($_POST["radio'$i'"].isset) {
+                    foreach ($answers as $answer) {
+                        if ($answer['AnswerText'] === $_POST["radio'$i'"]) {
+                            if ($answer['Correct'] === 1) {
+                           $score += $scorePerCorrect;
+                           $correctAnswers += "$i ";
+                            }
+                        } 
+                    }
+                }
+            }
+        }
+        ?>
+        <p> Score on Question <?php echo $question['QuestionId']; ?> <?php echo $score + "/" + $totalscore; ?>
+        <p> <?php echo $question['QuestionText']; ?> </p>
+      </div>
+    </div>
+    <?php include 'footer.php';?>
+  </body>
+</html>
