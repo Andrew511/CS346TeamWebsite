132bd3b855d343dadf95cef08f636b7b92860425
Andrew <wrege511@gmail.com>
2017-12-07 12:46:15 -0600

    Added missing semicolons

--- Private/PHP/queries.php
+++ Private/PHP/queries.php
@@ -453,7 +453,7 @@ function display_S_table() { //function to populate all the scores in the databa
 
   try{
     $query = "SELECT Score
-              FROM Scores;"
+              FROM Scores;";
     $stmt = $db->prepare($query);
     $stmt->execute();
     return $stmt->fetchall(PDO::FETCH_ASSOC);
@@ -468,7 +468,7 @@ function display_K_table() { //function to populate all the keywords in the data
 
   try{
     $query = "SELECT Keyword
-              FROM Keywords;"
+              FROM Keywords;";
     $stmt = $db->prepare($query);
     $stmt->execute();
     return $stmt->fetchall(PDO::FETCH_ASSOC);
