4613609801d9e176faaa12d3d82e9cfb0479e3ec
TChydro <33527113+TChydro@users.noreply.github.com>
2017-12-09 11:20:08 -0600

    updated session header

--- Public/PHP/ChangePassword.php
+++ Public/PHP/ChangePassword.php
@@ -1,12 +1,18 @@
 <?php
 session_start()  ;
-$dir = '/var/www/students/team6/CS346TeamWebsite/Private/PHP' ;
-$pdir = '/students/team6/CS346TeamWebsite/Public/PHP' ;
+$dir = realpath(__DIR__ . '/../..').'/Private/PHP' ;
+$pdir = dirname(__FILE__) ;
+$temp[] = preg_split("[/]" , $pdir) ;
+$pubDir = "";
+for($i = 3 ; $i < sizeof($temp[0]) ; $i++)
+{
+	$pubDir = $pubDir . "/" . $temp[0][$i] ;
+}
 require_once($dir.'/initialize.php') ;
 global $db ;
 if(!isset($_SESSION['ID']))
 	{
-		header("Location:" . $pdir . "/login.php") ;
+		header("Location:" . $pubDir . "/Login.php") ;
 	}
 	else
 	{
