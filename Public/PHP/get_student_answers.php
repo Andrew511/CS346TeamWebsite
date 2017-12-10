16c362f978113c128697984a994525785c41c3d0
guang16 <31829031+guang16@users.noreply.github.com>
2017-12-10 14:03:24 -0600

    syntax fix

--- Public/PHP/get_student_answers.php
+++ Public/PHP/get_student_answers.php
@@ -1,16 +1,16 @@
 <?php
-session_start()  ;
-$dir = realpath(__DIR__ . '/../..').'/Private/PHP' ;
-$pdir = dirname(__FILE__) ;
-$temp[] = preg_split("[/]" , $pdir) ;
-$pubDir = "";
-for($i = 3 ; $i < sizeof($temp[0]) ; $i++)
-{
-	$pubDir = $pubDir . "/" . $temp[0][$i] ;
-}
-require_once($dir.'/initialize.php') ;
-global $db ;
-if(!isset($_SESSION['ID']))
+	session_start()  ;
+	$dir = realpath(__DIR__ . '/../..').'/Private/PHP' ;
+	$pdir = dirname(__FILE__) ;
+	$temp[] = preg_split("[/]" , $pdir) ;
+	$pubDir = "";
+	for($i = 3 ; $i < sizeof($temp[0]) ; $i++)
+	{
+		$pubDir = $pubDir . "/" . $temp[0][$i] ;
+	}
+	require_once($dir.'/initialize.php') ;
+	global $db ;
+	if(!isset($_SESSION['ID']))
 	{
 		header("Location:" . $pubDir . "/Login.php") ;
 	}
@@ -20,7 +20,7 @@ if(!isset($_SESSION['ID']))
 		$id = $_SESSION['ID'] ;
 		$role = $_SESSION['role'] ;
 	}
-$q = get_active();
-$id = $q[0]["QuestionId"];
-echo json_encode(get_student_answers($id));
+	$q = get_active();
+	$id = $q[0]["QuestionId"];
+	echo json_encode(get_student_answers($id));
 ?>
